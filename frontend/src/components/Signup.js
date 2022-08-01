abfa4faa68cce3b196cc291f2b7a26a04cc352cf
Omar Alsakka <djomrofficial@gmail.com>
2022-08-01 18:21:16 +0300

    inputs check for signup done

--- frontend/src/components/Signup.js
+++ frontend/src/components/Signup.js
@@ -4,15 +4,14 @@ import UseField from "./UseField";
 import logo from "../media/logo-black.png";
 import { useState } from "react";
 import FormCheck from "../utils/FormCheck";
+import checkInputs from "../utils/InputChecks";
 
 const Signup = () => {
   const email = UseField("email");
   const username = UseField("text");
-  const firstname = UseField("text");
-  const lastname = UseField("text");
+  const fullname = UseField("text");
   const password = UseField("password");
-  const [consent, setConsent] = useState(true);
-
+  const [consent, setConsent] = useState(false);
   return (
     <Container className="signup-container">
       <Form>
@@ -30,16 +29,15 @@ const Signup = () => {
         <Form.Group className="mb-3">
           <Form.Label>Username</Form.Label>
           <Form.Control {...username} />
+          <Form.Text className="text-muted">
+            Username should contain letters and numbers only with minimum length
+            of 3
+          </Form.Text>
         </Form.Group>
 
         <Form.Group className="mb-3">
-          <Form.Label>First name</Form.Label>
-          <Form.Control {...firstname} />
-        </Form.Group>
-
-        <Form.Group className="mb-3">
-          <Form.Label>Last name</Form.Label>
-          <Form.Control {...lastname} />
+          <Form.Label>Full Name</Form.Label>
+          <Form.Control {...fullname} />
         </Form.Group>
 
         <Form.Group className="mb-3">
@@ -47,7 +45,7 @@ const Signup = () => {
           <Form.Control {...password} />
           <Form.Text className="text-muted">
             Password should contain at least 1 uppercase, 1 lowercase letter, 1
-            number and 1 special character
+            number and 1 special character. Minimum length 8.
           </Form.Text>
         </Form.Group>
 
@@ -63,7 +61,12 @@ const Signup = () => {
         </Form.Group>
 
         <Button
-          disabled={consent}
+          disabled={
+            checkInputs(username.value, password.value, fullname.value) &&
+            consent
+              ? false
+              : true
+          }
           className="form-button"
           variant="primary"
           type="submit"
---
+++ frontend/src/utils/InputChecks.js
@@ -0,0 +1,28 @@
+export const checkUserName = (username) => {
+  const valid = /^[0-9a-zA-Z]{3,}$/;
+  return valid.test(username);
+};
+
+export const checkPassword = (password) => {
+  const valid =
+    /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/;
+  return valid.test(password);
+};
+
+export const checkFullName = (fullname) => {
+  const valid = /^[a-zA-Z]+(\s[a-zA-Z]*){0,2}$/;
+  if (fullname.length >= 6 && fullname.length <= 20 && valid.test(fullname)) {
+    return true;
+  }
+  return false;
+};
+
+const checkInputs = (username, password, fullname) => {
+  return checkFullName(fullname) &&
+    checkPassword(password) &&
+    checkUserName(username)
+    ? false
+    : true;
+};
+
+export default checkInputs;
