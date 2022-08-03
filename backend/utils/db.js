c0bcd73174fab40b7b74417f8c87c1981cfa9390
Omar Alsakka <djomrofficial@gmail.com>
2022-08-03 11:51:08 +0300

    switched to using process.env

--- backend/utils/db.js
+++ backend/utils/db.js
@@ -1,11 +1,12 @@
 const POOL = require("pg").Pool;
+require("dotenv").config();
 
 const pool = new POOL({
-  user: "postgres",
-  password: "123456",
-  host: "localhost",
-  port: 5432,
-  database: "matcha",
+  user: process.env.DBUSER,
+  password: process.env.DBPASS,
+  host: process.env.DBHOST,
+  port: process.env.DBPORT,
+  database: process.env.DB,
 });
 
 module.exports = pool;
