e70a27a3d042138ed66473f243cf6a322e9bfa00
Luke <pilot@Luke.local>
2022-08-05 14:15:19 +0300

    refactoring, still figuring out how to return the value, having problems

--- frontend/src/utils/locationTools.js
+++ frontend/src/utils/locationTools.js
@@ -7,11 +7,21 @@ const reverseGeocoder = () => {
 	navigator.geolocation.getCurrentPosition((position) => {
 		let lat = position.coords.latitude;
 		let long = position.coords.longitude;
+
 		/* Geocode.setApiKey(process.env.GEOAPI);
 		const resp = Geocode.fromLatLng(lat, long);
 		console.log(resp); */
-	});
 
+		/* Geocode.fromLatLng(lat, long).then(
+			(response) => {
+				let address = response.results[0].formatted_address;
+				console.log(address)
+			},
+			(error) => {
+				console.error(error);
+			}
+		); */
+	});
 }
 
 export default reverseGeocoder;
