2afd3fcb2fdb8a45ea322e2eec7b028e1dc16981
Luke <pilot@Luke.local>
2022-08-05 13:53:47 +0300

    refactoring, still figuring out how to return the value, having problems

--- frontend/src/utils/locationTools.js
+++ frontend/src/utils/locationTools.js
@@ -3,26 +3,14 @@ import Geocode from "react-geocode";
 
 const reverseGeocoder = () => {
 
-	let lat;
-	let long;
-
-	navigator.geolocation.getCurrentPosition(function(position) {
-	  lat = position.coords.latitude;
-	  long = position.coords.longitude;
-	  
-	  Geocode.setApiKey(process.env.GEOAPI);
-	  let address;
- 
-	  Geocode.fromLatLng(lat, long).then(
-		  (response) => {
-			  address = response.results[0].formatted_address;
-			  return(address)
-		  },
-		  (error) => {
-			  console.error(error);
-		  }
-	  );
+	navigator.geolocation.getCurrentPosition((position) => {
+		let lat = position.coords.latitude;
+		let long = position.coords.longitude;
+		/* Geocode.setApiKey("AIzaSyB_TnuRBpNYHV-t01uetyK-VvkK572uHL4");
+		const resp = Geocode.fromLatLng(lat, long);
+		console.log(resp); */
 	});
+
 }
 
 export default reverseGeocoder;
