cae66d5f9e9b001653d1ef0b29789feb881d231c
Luke <pilot@Luke.local>
2022-08-05 12:51:04 +0300

    fixes done for reverse geocoding function

--- frontend/src/utils/locationTools.js
+++ frontend/src/utils/locationTools.js
@@ -1,20 +1,28 @@
 require("dotenv").config();
 import Geocode from "react-geocode";
 
-const reverseGeocoder = (lat, long) => {
+const reverseGeocoder = () => {
 
-	Geocode.setApiKey(process.env.GEOAPI);
-	let address;
+	let lat;
+	let long;
 
-	Geocode.fromLatLng(lat, long).then(
-		(response) => {
-			address = response.results[0].formatted_address;
-		},
-		(error) => {
-			console.error(error);
-		}
-	);
-	return(address);
+	navigator.geolocation.getCurrentPosition(function(position) {
+	  lat = position.coords.latitude;
+	  long = position.coords.longitude;
+	  
+	  Geocode.setApiKey(process.env.GEOAPI);
+	  let address;
+ 
+	  Geocode.fromLatLng(lat, long).then(
+		  (response) => {
+			  address = response.results[0].formatted_address;
+			  return(address)
+		  },
+		  (error) => {
+			  console.error(error);
+		  }
+	  );
+	});
 }
 
 export default reverseGeocoder;
