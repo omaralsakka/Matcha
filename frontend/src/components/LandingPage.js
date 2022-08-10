2c96f5a98cb60385c3ee1d3e399a3a99b66c00d1
Omar Alsakka <djomrofficial@gmail.com>
2022-08-10 11:08:40 +0300

    slide show for images

--- frontend/src/components/LandingPage.js
+++ frontend/src/components/LandingPage.js
@@ -1,15 +1,33 @@
 import { Button, Container } from "react-bootstrap";
-import mobImage from "../media/cp1.jpg";
+import img1 from "../media/cp1.jpg";
+import img2 from "../media/cp2.jpg";
+import img3 from "../media/cp3.jpg";
+import img4 from "../media/cp4.jpg";
 import logo from "../media/Matcha-logos_black.png";
 import { Link } from "react-router-dom";
+import { useEffect, useState } from "react";
 
 const LandingPage = () => {
+  const imgsArr = [img1, img2, img3, img4];
+  const [cardImg, setCardImg] = useState(imgsArr[0]);
+  let i = 0;
+
+  useEffect(() => {
+    setInterval(() => {
+      i += 1;
+      if (i > 3) {
+        i = 0;
+      }
+      setCardImg(imgsArr[i]);
+    }, 5000);
+  }, []);
+
   return (
     <div className="landing-page">
       <Container className="landing-page-content">
         <div className="image-container mx-5">
           <div className="image-div">
-            <img alt="" src={mobImage} className="mobile-Img"></img>
+            <img alt="" src={cardImg} className="mobile-Img"></img>
           </div>
         </div>
         <div className="center-items">
